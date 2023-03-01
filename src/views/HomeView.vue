4f26f1f02c0055639bd81b4da87a962edb374517
Yurii Rybachok <drummer2009@ukr.net>
2023-03-01 14:58:50 +0200

    add reusable component Card

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,10 @@
 <template>
-  <div>
-    <h1>Hello</h1>
+  <div class="home">
+    <div class="container">
+      <!-- Reviews Form -->
+      <!-- Reviews Stats -->
+      <!-- Reviews List -->
+    </div>
   </div>
 </template>
 
