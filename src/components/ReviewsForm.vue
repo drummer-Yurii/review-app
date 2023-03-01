7117ed7cb170df8273d952511eb4d587245a97c8
Yurii Rybachok <drummer2009@ukr.net>
2023-03-01 15:28:52 +0200

    add reviewForm component

--- src/components/ReviewsForm.vue
+++ src/components/ReviewsForm.vue
@@ -1,13 +1,23 @@
 <template>
-    <div>
-
-    </div>
+    <Card>
+        <form>
+            <h2>How would you rate your service with us?</h2>
+            <!-- Rating Component -->
+            <div class="input-group">
+                <input type="text" placeholder="Write a review" v-model="text">
+                <button type="submit" class="btn btn-primary" :disabled="btnDisabled"></button>
+            </div>
+            <div class="message">Text must be at least 10 characters</div>
+        </form>
+    </Card>
 </template>
 
 <script setup>
-
+import { ref } from 'vue';
+import Card from './shared/Card.vue';
+const text = ref('');
+const btnDisabled = ref(false);
+const message = ref('');
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style scoped></style>
--- src/components/shared/Card.vue
+++ src/components/shared/Card.vue
@@ -16,7 +16,7 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
 .card {
     background: #fff;
     color: #212121;
