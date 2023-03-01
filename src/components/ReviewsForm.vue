5e94a3637db2853658aa3a7c74c603111eb140ff
Yurii Rybachok <drummer2009@ukr.net>
2023-03-01 15:54:22 +0200

    add ratingSelect component

--- src/components/ReviewsForm.vue
+++ src/components/ReviewsForm.vue
@@ -3,6 +3,7 @@
         <form>
             <h2>How would you rate your service with us?</h2>
             <!-- Rating Component -->
+            <rating-select :rating="rating" @setRating="setRating" />
             <div class="input-group">
                 <input type="text" placeholder="Write a review" v-model="text">
                 <button type="submit" class="btn btn-primary" :disabled="btnDisabled"></button>
@@ -14,10 +15,16 @@
 
 <script setup>
 import { ref } from 'vue';
+import RatingSelect from './RatingSelect.vue';
 import Card from './shared/Card.vue';
 const text = ref('');
 const btnDisabled = ref(false);
 const message = ref('');
+const rating = ref(10);
+const setRating = (val) => {
+    rating.value = val;
+    console.log(val);
+};
 </script>
 
 <style scoped></style>
